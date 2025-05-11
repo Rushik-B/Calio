b125915d74540d2980d1dc7e5e154b164ffe8f19
Rushik-B <rushikbusiness@gmail.com>
2025-05-11 11:30:51 -0700

    Fixed backend es lint issues 16

--- apps/server/src/lib/prisma.ts
+++ apps/server/src/lib/prisma.ts
@@ -6,12 +6,12 @@ if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   // Ensure the prisma instance is re-used during hot-reloading
-  // @ts-expect-error
+  // @ts-expect-error - global.prisma is not typed on global
   if (!global.prisma) {
-    // @ts-expect-error
+    // @ts-expect-error - global.prisma is not typed on global
     global.prisma = new PrismaClient();
   }
-  // @ts-expect-error
+  // @ts-expect-error - global.prisma is not typed on global
   prisma = global.prisma;
 }
 
