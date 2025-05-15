28fc0dd0e8aa206d97ae36af699ffaf50a42c0ee
Rushik-B <rushikbusiness@gmail.com>
2025-05-15 14:38:45 -0700

    Fixed eslint issues

--- apps/server/src/app/api/calendar/hello/route.ts
+++
@@ -1,2 +0,0 @@
-// This file has been cleared as part of the reset.
-// You can implement calendar API endpoints here when ready. 
--- apps/server/src/app/api/process/route.ts
+++
@@ -1,2 +0,0 @@
-// This file has been cleared as part of the reset.
-// You can implement process API endpoints here when ready. 
